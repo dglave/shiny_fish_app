16b845b478dd94ef0212e9c2f486ea1871f24b78
grace-brofman <gracebrofman@bren.ucsb.edu>
2021-02-22 14:48:52 -0800

    working on stugg

--- shiny_fish_app/test_app/app.R
+++ shiny_fish_app/test_app/app.R
@@ -15,7 +15,7 @@ library(shinydashboard)
 library(shinythemes)
 library(hrbrthemes)
 
-fish <- read_csv("fish2.csv")
+fish <- data.table::fread("fish2.csv")
 
 my_theme <- bs_theme(
     bg = "red",
@@ -31,45 +31,49 @@ ui <- dashboardPage(skin = "blue",
                     dashboardHeader(title = "Grace & Dylan luv fish"),
                     dashboardSidebar(
                         sidebarMenu(id = "menu",
-                                    menuItem("Home",
-                                             tabname = "home_tab",
-                                             icon = icon("fas fa-globe")),
                                     menuItem("Fishermen",
                                              tabName = "fishermen_tab",
                                              icon = icon("fas fa-anchor")),
+                                    menuItem("Home",
+                                             tabname = "home_tab",
+                                             icon = icon("fas fa-globe")),
                                     menuItem("Fish",
                                              tabName = "fish_graph_tab",
                                              icon = icon("fish")))),
 
                     dashboardBody(
-                        fluidPage(theme = my_theme,
-                                  h1("Really important things about fish here"), #header on all tabs
-                                  p("Fish tend to have two eyes", #subheader on all tabs
-                                    a("What IS a fish exactly?", #subheader on all tabs
-                                      href = "https://en.wikipedia.org/wiki/Fish")#end of a
-                                    )#end of p
-                                  ),#end of fluidPage
+                        # fluidPage(theme = my_theme,
+                                  # h1("Really important things about fish here"), #header on all tabs
+                                  # p("Fish tend to have two eyes", #subheader on all tabs
+                                  #   a("What IS a fish exactly?", #subheader on all tabs
+                                  #     href = "https://en.wikipedia.org/wiki/Fish")#end of a
+                                  #   )#end of p
+                                  # ),#end of fluidPage
                         tabItems(
  # took this next tab from a different example - it's not showing up yet
-                                                       tabItem(
-                                tabName = "home_tab",
-                                HTML('<iframe width="560" height="315" src="https://www.youtube.com/watch?v=c9QM87Jl7k4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
-                                h3("About this app:"),
-                                p("This app will show blah blah blah")#end of p
-                            ),#end of tabItem1
                             tabItem(tabName = "fishermen_tab",
-                                    h3("Fish or not a fish:"),
+                                    h3("Fish or not a fish?"),
                                     p("Description blah blah text")#end of p
-                                    ),#end of tabItem2
+                            ),
+                            tabItem(tabName = "home_tab",
+                                    h3("I'm not showing up right now:"),
+                                    p("This app will show blah blah blah")#end of p
+                                     ),#end of tabItem1
+                            #end of tabItem2
                             tabItem(tabName = "fish_graph_tab",
                                     fluidRow(
                                         shinydashboard::box(title = "Catch sum by entity graph",
                                                             selectInput("fishing_entity_name",
-                                                                        "Choose entity name (country):",
+                                                                        label = h4("Choose entity name (country):"),
                                                                         choices = c(unique(fish$fishing_entity_name)#end of unique
-                                                                                    )#end of c
-                                                                        )#end of selectInput
-                                                            ),#end of box
+                                                                                    ),#end of c
+                                                                        selected = 1,
+                                                                        multiple = FALSE
+
+                                                                        ),#end of selectInput
+                                                            hr(),
+                                                            fluidRow(column(3, verbatimTextOutput("value"))# also added this hr section, not doing anything
+                                                            )),#end of box
                                         shinydashboard::box(plotOutput(outputId = "fish_plot")#end of plotOutput (here is where i ended in script, and it works!)
                                                             )#end of box
                                     )#end of fluidRow
@@ -93,7 +97,28 @@ server <- function(input, output) {
 
     }#end of reactive({})
     )#end of reactive
-}#end of first {}
+# where end of first {} used to be before adding output section
+
+# Create a reactive plot, which depends on 'species' widget selection:
+
+output$fish_plot <- renderPlot({
+
+  ggplot(data = fish_select(), aes(x = year, y = catch_sum)) +
+    geom_point() #removed point color aspect
+
+})
+
+output$value <- renderPrint({ input$fishing_entity_name }) # this is the last thing I added
+}
+
+
+
+#### End of sort-of-working app
+
+
+
+
+
 
 # ---------------------------------------------------------------------------
 # Create a reactive plot (this section doesn't work right now)
@@ -175,6 +200,9 @@ server <- function(input, output) {
 
 
 shinyApp(ui = ui, server = server)
+
+
+
 
 
 
